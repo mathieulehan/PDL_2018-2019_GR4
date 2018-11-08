88d84c07640803219815cf88ef6bbaff0f4c9b66
mathieulehan <mathieu.lehan@gmail.com>
2018-11-08 12:11:22 +0100

    Premiers tests

--- src/classes/ParseWikitext.java
+++ src/classes/ParseWikitext.java
@@ -1,3 +1,4 @@
+package classes;
 import java.io.FileWriter;
 import java.io.IOException;
 
---
+++ src/test/ParseHTMLTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import classes.ExtractWikitable;
+import classes.ParseHTML;
+
+class ParseHTMLTest {
+
+	@Test
+	void testRecupTables() {
+		ExtractWikitable testExtractTable = new ExtractWikitable();
+		StringBuilder contentBuilder = new StringBuilder();
+		try {
+		    BufferedReader in = new BufferedReader(new FileReader("test.html"));
+		    String str;
+		    while ((str = in.readLine()) != null) {
+		        contentBuilder.append(str);
+		    }
+		    in.close();
+		} catch (IOException e) {
+		}
+		String html = contentBuilder.toString();
+		ParseHTML testParseHTML = new ParseHTML(html);
+		testParseHTML.htmlToCSV(html, "C:\\Users\\mathi\\Documents\\wikitext.csv");
+	}
+
+}
