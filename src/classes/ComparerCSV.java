82148558b85b465fbf349b72f593169d1d8ea380
Mathieu LE Han <mathieu.lehan@gmail.com>
2018-11-15 09:56:46 +0100

    ComparerCSV de base

--- src/classes/ComparerCSV.java
+++ src/classes/ComparerCSV.java
@@ -1,5 +1,21 @@
 package classes;
-
+/**
+ * Classe permettant d'effectuer diverses comparaison entre les deux méthodes de récupération de table wikipedia
+ * @author mathi
+ *
+ */
 public class ComparerCSV {
 
+	public boolean comparaisonTempsExecution(Donnee_Html html, Donnee_Wikitable wikitable) {
+		return html.getTime() < wikitable.getTime();
+	}
+	
+	public boolean comparaisonDonneesTableau(Donnee_Html html, Donnee_Wikitable wikitable) {
+		return false;
+	}
+	
+	public Donnee meilleurFormat(Donnee_Html html, Donnee_Wikitable wikitable) {
+		return wikitable;
+	}
+	
 }
