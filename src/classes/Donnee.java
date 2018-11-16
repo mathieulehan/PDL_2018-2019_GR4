701b2d89d8b7e9ea66a44fd4548bcc70ea0bf6e2
Charlotte <charlotte.laurensan@gmail.com>
2018-11-16 12:13:36 +0100

    Création exception "ConvertionInvalideException"

--- src/classes/Donnee.java
+++ src/classes/Donnee.java
@@ -3,10 +3,12 @@ package classes;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.json.JSONException;
 
+import exceptions.ConvertionInvalideException;
 import exceptions.ExtractionInvalideException;
 import exceptions.UrlInvalideException;
 
@@ -17,37 +19,42 @@ import exceptions.UrlInvalideException;
  */
 
 abstract class Donnee{
-		
+
 	private long tempsOriginal;
-	
+
 	/**
 	 * Extraction des donnees
 	 * @param url
-	 * @throws IOException
+	 * @throws UrlInvalideException
 	 * @throws ExtractionInvalideException 
-	 * @throws JSONException 
+	 * @throws MalformedURLException 
+	 * @throws ConvertionInvalideException 
 	 */
-	abstract void extraire(Url url) throws IOException, UrlInvalideException, ExtractionInvalideException, JSONException;
-	
+	abstract void extraire(Url url) throws UrlInvalideException, ExtractionInvalideException, MalformedURLException, ConvertionInvalideException;
+
 	/**
 	 * A partir de l'url donnee, recupere le contenu de la page en json
 	 * @param url
 	 * @return String
-	 * @throws IOException
+	 * @throws ExtractionInvalideException 
 	 */
-	public String recupContenu(URL url) throws IOException {
-		StringBuilder result = new StringBuilder();
-		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+	public String recupContenu(URL url) throws ExtractionInvalideException{
+		try {
+			StringBuilder result = new StringBuilder();
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
-		String inputLine;
+			String inputLine;
 
-		while ((inputLine = in.readLine()) != null)
-			result.append(inputLine);
+			while ((inputLine = in.readLine()) != null)
+				result.append(inputLine);
 
-		in.close();
-		return result.toString();
+			in.close();
+			return result.toString();
+		} catch (Exception e) {
+			throw new ExtractionInvalideException("Recuperation du contenu impossible");
+		}
 	}
-	
+
 	/**
 	 * Verification de la presence de tableaux
 	 * @param donnee
