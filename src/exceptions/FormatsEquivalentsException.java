48ac06247bff08b740b5020444046b3862a904dc
Charlotte <charlotte.laurensan@gmail.com>
2018-11-19 11:19:32 +0100

    Nouvelle Exception "FormatsEquivalentsException"

---
+++ src/exceptions/FormatsEquivalentsException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+/**
+ * 
+ * Classe creant une Exception lorsque la comparaison des 2 extraction (format HTML et Wikitext) :
+ * - Nombre de lignes equivalentes
+ * - Nombre de colonnes equivalentes
+ * @author Charlotte
+ *
+ */
+public class FormatsEquivalentsException extends Exception{
+
+	String message;
+	
+	public FormatsEquivalentsException(String message) {
+		super();
+		this.message = message;
+	}
+}
+
